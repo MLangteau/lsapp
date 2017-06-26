7fb956e66e609b1470beac719c8b7bedc139c351
MLangteau <langteauM@gmail.com>
2017-06-26 16:09:54 -0400

    added edit/update

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -83,7 +83,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -95,7 +96,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'ptitle' => 'required',
+            'body'  => 'required'
+        ]);
+
+        //  Find the Post
+        $post = Post::find($id);
+        $post->ptitle = $request->input('ptitle');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,9 @@
     <a href="/posts/" class="btn btn-default">Go Back</a>
         <h1>{{$post->ptitle}}</h1>
         <div>
-            {{$post->body}}<hr>
+            {!!$post->body!!}<hr>
         </div>
         <small>Written on {{$post->created_at}}</small>
+        <hr>
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 @endsection
