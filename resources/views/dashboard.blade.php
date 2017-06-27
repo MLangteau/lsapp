c882597126234a5a9b76855caf5cff972f89ae16
MLangteau <langteauM@gmail.com>
2017-06-27 11:41:22 -0400

    added if posts > 0 and edit and delete buttons to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,20 +10,31 @@
                 <div class="panel-body">
                     <a href="/posts/create" class="btn btn-primary">Create Post!</a>
                     <h3>Your Blog Posts</h3>
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Title</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        @foreach($posts as $post)
+                    {{--if no posts, display error message--}}
+                    @if(count($posts) > 0)
+                        <table class="table table-striped">
                             <tr>
-                                <th>{{$post->ptitle}}</th>
-                                <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default"></a></th>
+                                <th>Title</th>
+                                <th></th>
                                 <th></th>
                             </tr>
-                        @endforeach
-                    </table>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td>{{$post->ptitle}}</td>
+                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                    <td>
+                                    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                    {{Form::hidden('_method', 'DELETE')}}
+                                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    {!!Form::close()!!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have No Posts</p>
+                    @endif
+
                 </div>
             </div>
         </div>
