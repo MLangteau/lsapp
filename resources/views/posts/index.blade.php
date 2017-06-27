c17b0cc0f1d10c9d31c599b54dee584af74ec14d
MLangteau <langteauM@gmail.com>
2017-06-26 23:32:55 -0400

    added user name to posts (title) on index and show

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,8 +8,7 @@
         @foreach($posts as $post)
             <div class="well">
                 <h3><a href="/posts/{{$post->id}}">{{$post->ptitle}}</a></h3>
-                <h3>{{$post->body}}</h3>
-                <small>Written on {{$post->created_at}}</small>
+                <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
             </div>
         @endforeach
         {{$posts->links()}}
