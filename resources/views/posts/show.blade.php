e67cfab1f6e792c60ef5ef5df2fc73225dc96349
MLangteau <langteauM@gmail.com>
2017-06-27 11:29:02 -0400

    fixed href for posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts/" class="btn btn-default">Go Back</a>
+    <a href="/posts" class="btn btn-default">Go Back</a>
         <h1>{{$post->ptitle}}</h1>
         <div>
             {!!$post->body!!}<hr>
